71791d30b3ec94255db4ea4960d08e98329aba70
Joan Uribe <75236125+JoeUri@users.noreply.github.com>
2022-08-25 03:08:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//just a comments
+
+//define a variable and place into it the http package (shared code) from node.js
+let myhttp = require("http");
+
+//use dot notation in javascript to access an object and its methods (commands) and properties
+
+let myserver = myhttp.createServer(
+  function( myrequest, myresponse ){
+    console.log( myrequest.url );
+
+    let mytext;
+    if(myrequest.url === "/hey"){
+       mytext = "Well hello there...";
+    }else{
+       mytext = "I don't know you!";
+    }
+
+    myresponse.writeHead( 200, {"Content-Type": "text/plain"});
+
+    myresponse.end(mytext + "\n");
+  }
+);
+
+myserver.listen(8080, "0.0.0.0");
+
+console.log("server has started");
